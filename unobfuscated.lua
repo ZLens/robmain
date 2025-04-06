19faafd7034ed142b3fe3e585613d48ff85fed83
rv_ts <zackcairns4@icloud.com>
2025-04-06 14:12:49 +0100

    Update unobfuscated.lua

--- unobfuscated.lua
+++ unobfuscated.lua
@@ -1,3 +1,5 @@
+--  Protected by DMCA — violations will be met with immediate legal enforcement.
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
